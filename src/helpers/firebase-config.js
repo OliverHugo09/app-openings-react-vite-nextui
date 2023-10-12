4c11ce683eb2c44139336ee570e4dba55d59a3bd
OliverHugo09 <77972247+OliverHugo09@users.noreply.github.com>
2023-10-12 15:01:23 -0500

    DELETE firebase-config.js

--- src/helpers/firebase-config.js
+++ src/helpers/firebase-config.js
@@ -6,12 +6,6 @@ import { getFirestore } from "@firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAR0y4SWY7hsUwTjzvq7RK0IbCCZCGyk3c",
-    authDomain: "openings-cloud-react.firebaseapp.com",
-    projectId: "openings-cloud-react",
-    storageBucket: "openings-cloud-react.appspot.com",
-    messagingSenderId: "311170324177",
-    appId: "1:311170324177:web:ffb27e8611eaea24b0d7f0"
 };
 
 // Initialize Firebase
