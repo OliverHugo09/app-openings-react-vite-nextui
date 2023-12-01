2ddf511628987df5ebf4694a6b1b4c9fc3c629ea
BuildTools <1902343@utrivieramaya.edu.mx>
2023-12-01 16:02:12 -0500

    fix route card error

--- src/components/CardOpening.jsx
+++ src/components/CardOpening.jsx
@@ -1,29 +1,30 @@
-import { Card, CardHeader, CardBody, Image, Link } from "@nextui-org/react"
-import { useOpenings } from "../context/OpeningsProvider"
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import { Link } from "react-router-dom"; // Importa el componente Link de react-router-dom
+import { useOpenings } from "../context/OpeningsProvider";
 
 export const CardOpening = () => {
-
-    const { openings } = useOpenings()
+    const { openings } = useOpenings();
 
     return (
         <>
             {openings.map((opening) => (
-                <Card className="py-4" key={opening.id} as={Link} href={`/opening/${opening.id}`}>
-                    <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                        <p className="text-tiny uppercase font-bold">{opening.title}</p>
-                        <h4 className="font-bold text-large">{opening.anime}</h4>
-                    </CardHeader>
-                    <CardBody className="overflow-visible py-2 place-items-center">
-                        <Image
-                            alt="Card background"
-                            className="object-cover"
-                            src={opening.img}
-                            width={300}
-                        />
-                    </CardBody>
-                </Card>
-
+                <Link to={`/opening/${opening.id}`} key={opening.id}>
+                    <Card className="py-4">
+                        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                            <p className="text-tiny uppercase font-bold">{opening.title}</p>
+                            <h4 className="font-bold text-large">{opening.anime}</h4>
+                        </CardHeader>
+                        <CardBody className="overflow-visible py-2 place-items-center">
+                            <Image
+                                alt="Card background"
+                                className="object-cover"
+                                src={opening.img}
+                                width={300}
+                            />
+                        </CardBody>
+                    </Card>
+                </Link>
             ))}
         </>
-    )
-}
+    );
+};
--- src/components/FormAddOpening.jsx
+++ src/components/FormAddOpening.jsx
@@ -142,7 +142,7 @@ export const FormAddOpening = () => {
                                         name="img"
                                         id="img"
                                         autoComplete="img"
-                                        maxLength={60}
+                                        maxLength={1500}
                                         value={formData.img}
                                         onChange={(e) => setFormData({ ...formData, img: e.target.value })}
                                         className="block w-full rounded-md border-0 py-2 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
